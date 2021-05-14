d3a362dce3e23601733e87d7f64360994378f65a
Ronald Joseph <rexrony@users.noreply.github.com>
2021-05-14 13:12:42 +0500

    oware html
    oware html

---
+++ assets/js/functions.js
@@ -0,0 +1,139 @@
+// sticky header
+/*$(window).scroll(function() {    
+var scroll = $(window).scrollTop();
+//>=, not <=
+if (scroll >= 300) {
+//clearHeader, not clearheader - caps H
+$("header").addClass("stickyheader");
+} else {
+$("header").removeClass("stickyheader");  
+}
+});*/
+// sticky header end
+
+// wow start
+new WOW().init();
+// wow end
+
+// sticky social
+$(document).scroll(function () {
+	"use strict";
+    var y = $(this).scrollTop();
+    if (y > 200) {
+        $('.sticky-container').fadeIn();
+    } else {
+        $('.sticky-container').fadeOut();
+    }
+
+}); 
+// sticky social end
+
+$(document).ready(function() {
+"use strict";
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 40) {
+       $('.mobile-nav-btn').addClass('dsply-none-mob');
+    }
+    else {
+       $('.mobile-nav-btn').removeClass('dsply-none-mob');
+    }
+});
+// Active Current Page
+var str=location.href.toLowerCase();
+$(".nav-area-full a").each(function() {
+if (str.indexOf(this.href.toLowerCase()) > -1) {
+$(".active").removeClass("active");
+$(this).parent().addClass("active");
+}
+});	
+// Active Current Page end
+
+   //*****************************
+    // Mobile Navigation
+    //*****************************
+    $('.mobile-nav-btn').click(function() {
+        $('.mobile-nav-btn, .mobile-nav, .app-container').toggleClass('active');
+    });   
+
+    $('.firstlevel li a').click(function() {
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $(this).siblings('ul').slideUp();
+        }else{
+            $('.firstlevel li a').removeClass('active');
+            $(this).addClass('active');
+            $('.dropdown').slideUp();
+            $(this).siblings('ul').slideDown();
+        }
+    });
+
+    $('.mainnav > li > a').click(function() {
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $(this).parents('li').children('.firstlevel').slideUp();
+        }else{
+            $(this).addClass('active');
+            $(this).parents('li').children('.firstlevel').slideDown();
+            $(this).parents('li').siblings('li').children('a').removeClass('active');
+            $(this).parents('li').siblings('li').children('.firstlevel').slideUp();
+        }
+    }); 
+    
+////// main slider
+ $(".home-slider").slick({
+    dots: true,
+    arrows: true,
+    infinite: !0,
+    speed: 1000,
+    slidesToShow: 1,
+    autoplay: !0,
+    autoplaySpeed: 4000,
+    adaptiveHeight: !0,
+    responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+    dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+    });
+
+
+////// main slider end
+
+  ////// mob slider
+    $(".sliderxs").slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+		adaptiveHeight: true,
+        responsive: [
+            {
+              breakpoint: 10000,
+              settings: "unslick"
+            },
+            {
+              breakpoint: 767,
+              settings: {
+                unslick: true
+              }
+            }
+        ]
+    });
+////// mob slider end
+
+
+
+
+
+
+
+
+
+}); // document ready end
+
+ 
